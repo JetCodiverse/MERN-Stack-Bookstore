65fd16c6b07be20e4d7a836ee88a3b0129b8e61c
JetCodiverse <125890150+JetCodiverse@users.noreply.github.com>
2023-12-19 16:14:34 +0800

    Update CreateBook.jsx
    update link from localhost to https://mern-stack-bookstore-api.vercel.app/

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -21,7 +21,7 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5555/books", data)
+      .post("https://mern-stack-bookstore-api.vercel.app/", data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Created successfully", { variant: "success" });
